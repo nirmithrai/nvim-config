e633f80991c39637ee680a034266b072aab34e92
nirmith <nirmithrai@gmail.com>
2025-04-23 14:05:07 +0530

    tab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local o = vim.opt
+
+-- Use real tabs, not spaces
+o.expandtab = false -- <Tab> inserts a tabchar, not spaces
+o.shiftwidth = 4 -- width for auto-indent and >> <<
+o.tabstop = 4 -- a literal Tab is displayed as 4 spaces wide
+o.softtabstop = 4 -- <BS> in insert removes a real tab as 4 spaces
